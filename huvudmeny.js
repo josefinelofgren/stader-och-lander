a53aa0ee598f1eb3c2d60d29fe3cb34efc721c6d
Gabrielle Kamph <gabrielle.kamph@gmail.com>
2021-02-17 21:15:10 +0100

    Making countries clickable

--- huvudmeny.js
+++ huvudmeny.js
@@ -9,40 +9,43 @@ Promise.all([
         console.log("Länder", data[0]);
         console.log("Städer", data[1]);
         printCountries(data);
-        printCities(data);
+        // printCities(data);
     })
+    
 
 // Funktion för att skriva ut länder i huvudmenyn
 
 function printCountries(list) {
 
     let countries = list[0];
+    let cities = list[1];
+
     for (let country in countries) {
         if (countries.hasOwnProperty(country)) {
-            console.log(countries[country].countryname);
-            document.getElementById("listCountries").insertAdjacentHTML("beforeend", "<ul>" + countries[country].countryname + "</ul>");
+            // console.log(countries[country].countryname);
+            document.getElementById("listCountries").insertAdjacentHTML("beforeend", "<ul><li id='"+ countries[country].id +"'>" + countries[country].countryname + "</li></ul>");
         }  
     }
+
+    // När man klickar på någon av länderna så skapas en <li> med dess städer    
+
+    listCountries.addEventListener("click", function(evt) {
+        listCountries.appendChild(listCities);
+        listCities.innerHTML = "";
+
+        for (let city in cities) {
+
+            if (evt.target.id == cities[city].countryid) {
+                // console.log(cities[city].stadname);
+                listCities.insertAdjacentHTML("beforeend", "<li>" + cities[city].stadname + "</li>");
+            }
+        }
+    });
 }
 
-// Lista med städerna
-function printCities(list) {
-    let cities = list[1];
-    let countries = list[0];
-    console.log(countries);
-    for (let city in cities) {
-        if (cities[city].countryid == 2){
-        console.log(cities[city].stadname);
-        listCities.insertAdjacentHTML("beforeend", "<li>" + cities[city].stadname + "</li>");
-    }
-    }
-}
 
-// När man klickar på någon av länderna så aktiveras printCities()
-listCountries.addEventListener("click", function () {
-    listCountries.appendChild(listCities);
-    console.log("click");
-});
+
+
 
 
 
