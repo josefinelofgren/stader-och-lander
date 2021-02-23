8198e614cbd3b68a81b788f057bcf1b2b554675f
unknown <zlinus.zachrison@gmail.com>
2021-02-23 10:40:32 +0100

    changing wiki api from english to swedish

--- modules/städer.mjs
+++ modules/städer.mjs
@@ -44,7 +44,7 @@ export function displayCityInfo(id) {
           ).nextElementSibling.innerHTML += `<p> <h4>Väder:</h4> ${data.data[0].app_temp} C, ${data.data[0].weather.description} </p>`;
 
           return fetch(
-            `https://en.wikipedia.org/api/rest_v1/page/summary/${selectedCity.stadname} `
+            `https://sv.wikipedia.org/api/rest_v1/page/summary/${selectedCity.stadname} `
           )
             .then((response) => response.json())
             .then((data) => {
