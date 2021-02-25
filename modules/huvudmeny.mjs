4c1c7cff14fe6ef8350918b3a239880295b9ccb4
unknown <zlinus.zachrison@gmail.com>
2021-02-25 12:18:54 +0100

    fixing bug on menu

--- modules/huvudmeny.mjs
+++ modules/huvudmeny.mjs
@@ -1,4 +1,4 @@
-import {displayCityInfo} from "./städer.mjs"
+import { displayCityInfo } from "./städer.mjs";
 
 // Variables
 let listCountries = document.getElementById("listCountries");
@@ -9,95 +9,109 @@ let cityInfo = document.getElementById("CityInfo");
 
 // Fetch json-files
 export let fetchData = Promise.all([
-    fetch("json/land.json").then(response => response.json()),
-    fetch("json/stad.json").then(response => response.json())
-])
-    .then(data => {
-        console.log("Länder", data[0]);
-        console.log("Städer", data[1]);
-        printCountries(data);
-        printCityInformation(data);
-    })
+  fetch("json/land.json").then((response) => response.json()),
+  fetch("json/stad.json").then((response) => response.json()),
+]).then((data) => {
+  console.log("Länder", data[0]);
+  console.log("Städer", data[1]);
+  printCountries(data);
+  printCityInformation(data);
+});
 
 // Print countries in headmenu
 export function printCountries(list) {
-    let countries = list[0];
-    let cities = list[1];
+  let countries = list[0];
+  let cities = list[1];
 
-
-    for (let country in countries) {
-        if (countries.hasOwnProperty(country)) {
-            document.getElementById("listCountries").insertAdjacentHTML("beforeend", "<ul><li class='listCountries' id='" + countries[country].id + "'data-type='country'>" + countries[country].countryname + "</li></ul>");
-            listCountries.setAttribute("type", "country");
-        }
+  for (let country in countries) {
+    if (countries.hasOwnProperty(country)) {
+      document
+        .getElementById("listCountries")
+        .insertAdjacentHTML(
+          "beforeend",
+          "<ul><li class='listCountries' id='" +
+            countries[country].id +
+            "'data-type='country'>" +
+            countries[country].countryname +
+            "</li></ul>"
+        );
+      listCountries.setAttribute("type", "country");
     }
+  }
 
-    // Show cities while clicking a specific country
-    listCountries.addEventListener("click", function (evt) {
-        listCountries.appendChild(listCities);
-        listCities.innerHTML = "";
-        var type = evt.target.getAttribute("data-type");
+  // Show cities while clicking a specific country
+  listCountries.addEventListener("click", function (evt) {
+    listCountries.appendChild(listCities);
 
-        if (type == "country") {
+    var type = evt.target.getAttribute("data-type");
 
-            for (let city in cities) {
-                if (evt.target.id == cities[city].countryid) {
-                    listCities.insertAdjacentHTML("beforeend", "<li class='listCities' id='" + cities[city].id + "' value='" + cities[city].population + "'>" + cities[city].stadname + "</li>");
-                }
-            }
+    if (type == "country") {
+      listCities.innerHTML = "";
 
+      for (let city in cities) {
+        if (evt.target.id == cities[city].countryid) {
+          listCities.insertAdjacentHTML(
+            "beforeend",
+            "<li class='listCities' id='" +
+              cities[city].id +
+              "' value='" +
+              cities[city].population +
+              "'>" +
+              cities[city].stadname +
+              "</li>"
+          );
         }
-    });
+      }
+    }
+  });
 }
 
 // Print city information (name, population and info)
 export function printCityInformation() {
+  listCities.addEventListener("click", function (evt) {
+    document.getElementById("savedCities").style.display = "none";
+    document.getElementById("visitedCity").style.display = "inline-block";
+    document.getElementById("viewVisitedCities").style.display = "block";
 
-    listCities.addEventListener("click", function (evt) {
-        document.getElementById("savedCities").style.display = "none";
-        document.getElementById("visitedCity").style.display = "inline-block";
-        document.getElementById("viewVisitedCities").style.display ="block";
-        
-        evt.stopPropagation();
-        let cityTitle = evt.target.innerHTML;
-        let cityPopulation = evt.target.value;
+    evt.stopPropagation();
+    let cityTitle = evt.target.innerHTML;
+    let cityPopulation = evt.target.value;
 
-        if (cityPopulation == 0) {
-            cityPopulation = "Uppgift saknas!";
-        }
+    if (cityPopulation == 0) {
+      cityPopulation = "Uppgift saknas!";
+    }
 
-        // Print weather, img and info 
-        displayCityInfo(evt.target.id);
+    // Print weather, img and info
+    displayCityInfo(evt.target.id);
 
-        document.getElementById("cityName").innerHTML = ("<h1 class='city-name' id=" + evt.target.id + ">" + cityTitle + "</h1>");
-        document.getElementById("cityInfo").innerHTML = ("<p class='population'> Antal invånare: " + cityPopulation + "</p>");
- });
+    document.getElementById("cityName").innerHTML =
+      "<h1 class='city-name' id=" + evt.target.id + ">" + cityTitle + "</h1>";
+    document.getElementById("cityInfo").innerHTML =
+      "<p class='population'> Antal invånare: " + cityPopulation + "</p>";
+  });
 }
- 
+
 // Funktion som sparar stadens ID i local storage när man klickar på "besökt"-knappen
- 
 
-document.getElementById("visitedCity").addEventListener("click", saveLocalStorage);
+document
+  .getElementById("visitedCity")
+  .addEventListener("click", saveLocalStorage);
 export function saveLocalStorage(evt) {
+  let cityId = document.getElementsByClassName("city-name")[0].id;
+  console.log("Stadens id: " + cityId);
 
-    let cityId = document.getElementsByClassName("city-name")[0].id;
-    console.log("Stadens id: " + cityId);
-    
-
-    if (localStorage) {
+  if (localStorage) {
     let citiesArray;
-    
+
     if (!localStorage["savedID"]) citiesArray = [];
-    
     else citiesArray = JSON.parse(localStorage["savedID"]);
     localStorage.clear();
 
-
-    if (citiesArray.includes(cityId)){
-        console.log("property exists");
-    } else{
-        citiesArray.push(cityId);
+    if (citiesArray.includes(cityId)) {
+      console.log("property exists");
+    } else {
+      citiesArray.push(cityId);
     }
     localStorage.setItem("savedID", JSON.stringify(citiesArray));
-    }
- };
+  }
+}
